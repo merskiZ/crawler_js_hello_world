a22191675bd92d2a3c20a11f4f922ff007aaa30c
Yameng Zhou <merski.mobile@gmail.com>
2022-04-11 22:27:51 -0700

    first commit for the hello world project

---
+++ src/crawler.js
@@ -0,0 +1,6 @@
+var request = require('request')
+var cheerio = require('cheerio')
+var URL = require('url-parse')
+
+var pageToVisit = "http://www.arstechnica.com"
+console.log("Visiting page" + pageToVisit)
